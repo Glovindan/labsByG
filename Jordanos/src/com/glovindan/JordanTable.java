711b526f4fbd494cfc4fc8295a26ab83d7c37dc6
glovindan <turbo69nice@gmail.com>
2021-10-04 17:36:33 +0500

    i did a shit

--- Jordanos/src/com/glovindan/JordanTable.java
+++ Jordanos/src/com/glovindan/JordanTable.java
@@ -1,5 +1,7 @@
 package com.glovindan;
 
+import java.util.Arrays;
+
 public class JordanTable {
     private double[][] tValues;
     private String[] tRow;
@@ -11,23 +13,56 @@ public class JordanTable {
             tValues[row] = inputValues[row].clone();
         }
 
-        this.tRow = new String[inputValues.length];
-        for (int column = 0; column < this.tRow.length; column++) {
-            this.tRow[column] = "x" + (column + 1);
+        this.tRow = new String[inputValues[0].length];
+        this.tRow[0] = "1";
+        for (int column = 1; column < this.tRow.length; column++) {
+            this.tRow[column] = "x" + (column);
         }
 
-        this.tColumn = new String[inputValues[0].length];
-        for (int row = 0; row < this.tColumn.length; row++) {
-            this.tColumn[row] = "y" + (row + 1);
-        }
+        this.tColumn = new String[inputValues.length];
+        Arrays.fill(this.tColumn, "0");
     }
 
     public double[][] getValues() {
         return tValues;
     }
 
+    public String[][] getTableAsStringMatrix() {
+        String[][] tableToString = new String[this.tColumn.length + 1][this.tRow.length + 1];
+
+        //0 0
+        tableToString[0][0] = " ";
+
+        //Значения нулевой строки
+        for (int column = 1; column <= this.tRow.length; column++) {
+            tableToString[0][column] = "-" + this.tRow[column - 1];
+        }
+        //Значения нулевого столбца
+        for (int row = 1; row <= this.tColumn.length; row++) {
+            tableToString[row][0] = this.tColumn[row - 1];
+        }
+        //Значения таблицы
+        for (int row = 1; row <= this.tColumn.length ; row++) {
+            for (int column = 1; column <= this.tRow.length; column++) {
+                tableToString[row][column] = Double.toString(this.tValues[row - 1][column - 1]);
+            }
+        }
+
+        return tableToString;
+    }
+
+    public static void printDoubleMatrix(double[][] matrix) {
+        for (double[] doubles : matrix) {
+            for (double aDouble : doubles) {
+                System.out.printf("%10.1f", aDouble);
+            }
+            System.out.println();
+        }
+    }
+
     public boolean jordanExclude(int x, int y) {
         double permittedElement = tValues[x][y];
+        double[][] newTable = new double[this.tColumn.length][this.tRow.length];
 
         if(permittedElement == 0) return false;
 
@@ -37,26 +72,27 @@ public class JordanTable {
         tColumn[x] = tRow[y];
         tRow[y] = buffer;
 
-        for (int row = 0; row < tRow.length; row++) {
-            for (int column = 0; column < tColumn.length; column++) {
+        for (int row = 0; row < tColumn.length; row++) {
+            for (int column = 0; column < tRow.length; column++) {
                 if(row != x && column != y) {
-                    tValues[row][column] = tValues[row][column] - (tValues[row][y] * tValues[x][column])/(permittedElement);
+                    newTable[row][column] = (tValues[row][column] * permittedElement - tValues[row][y] * tValues[x][column])/permittedElement;
                 }
             }
         }
 
         for (int column = 0; column < tRow.length; column++) {
-            if(column != x) {
-                tValues[0][column] = - tValues[0][column]/permittedElement;
+            if(column != y) {
+                newTable[x][column] =  tValues[x][column]/permittedElement;
             }
         }
 
-        for (int row = 0; row < tRow.length; row++) {
+        for (int row = 0; row < tColumn.length; row++) {
             if(row != x) {
-                tValues[row][0] = tValues[row][0]/permittedElement;
+                newTable[row][y] = - tValues[row][y]/permittedElement;
             }
         }
 
+        this.tValues = newTable;
         return true;
     }
 }
--- Jordanos/src/com/glovindan/Main.java
+++ Jordanos/src/com/glovindan/Main.java
@@ -1,10 +1,8 @@
 package com.glovindan;
 
-import java.util.Arrays;
-
 public class Main {
 
-    public static void printMatrix(double[][] matrix) {
+    public static void printDoubleMatrix(double[][] matrix) {
         for (double[] doubles : matrix) {
             for (double aDouble : doubles) {
                 System.out.printf("%10.1f", aDouble);
@@ -13,26 +11,56 @@ public class Main {
         }
     }
 
+    public static void printStringMatrix(String[][] matrix) {
+        for (String[] strings : matrix) {
+            for (String aStrings : strings) {
+                System.out.printf("%10.8s", aStrings);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void makeJordanosShit(JordanTable A, int x, int y) {
+        System.out.println("Old Matrix");
+        printStringMatrix(A.getTableAsStringMatrix());
+
+        boolean status = A.jordanExclude(x,y);
+        if (status) {
+            System.out.println("New Matrix");
+            printStringMatrix(A.getTableAsStringMatrix());
+        } else {
+            System.out.println("Ошибка");
+        }
+    }
+
     public static void main(String[] args) {
 
         //Ввод матрицы
+//        double[][] initMatrix = {
+//            {3.0, 1.0, 2.0, 1.0, 0.0},
+//            {6.0, 1.0, 1.0, 0.0, 1.0},
+//            {10.0, 1.0, -1.0, -2.0, -3.0}
+//        };
+//        double[][] initMatrix = {
+//                {2, -3, 1},
+//                {3, 2, 2},
+//                {5, -1, 3}
+//        };
         double[][] initMatrix = {
-            {0.0, -3.0, 1.0},
-            {3.0, 2.0, 2.0},
-            {5.0, -1.0, 3.0}
+                {4.0, 1.0, 2.0, 1.0, 0.0},
+                {6.0, 1.0, 1.0, 0.0, 1.0},
+                {10.0, 1.0, -1.0, -2.0, 3.0}
         };
-
         JordanTable A = new JordanTable(initMatrix);
-        boolean status = A.jordanExclude(0,0);
-        double[][] newMatrix = A.getValues();
+        makeJordanosShit(A, 1, 4);
 
-        System.out.println("Old Matrix");
-        printMatrix(initMatrix);
-        if (status) {
-            System.out.println("New Matrix");
-            printMatrix(newMatrix);
-        } else {
-            System.out.println("ты проебался");
-        }
+        double[][] secondMatrix = {
+                {1.0, 1.0, 2.0, 3.0},
+                {1.0, 1.0, 1.0, 1.0},
+                {2.0, 1.0, 0.0, -1.0}
+        };
+        JordanTable B = new JordanTable(secondMatrix);
+        makeJordanosShit(B, 2, 1);
+        makeJordanosShit(B, 1, 2);
     }
 }
